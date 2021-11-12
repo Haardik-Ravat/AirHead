f12a472debba904ff1103110c037b0c2514667ec
Haardik <ravat.1@iitj.ac.in>
2021-11-12 10:10:11 +0530

    Base setup and Bot created

---
+++ main.py
@@ -0,0 +1,7 @@
+import discord
+from discord.ext import commands
+
+client=commands.Bot(command_prefix=">", intents=discord.Intents.all())
+
+client.run("OTA4NTc0NzkzNDE3MjYxMDU3.YY3uNw.hmuW3WDpjZq_X9ilq4z_QkNEzmo")
+
