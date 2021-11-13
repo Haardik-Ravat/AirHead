6f6fd6fd51dc4691d21f65502ab66df9ee239191
Haardik <ravat.1@iitj.ac.in>
2021-11-13 23:59:22 +0530

    additional changes

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import discord
 from discord.ext import commands
+import music
+
+cogs=[music]
+
+
+
+
+
 
 client=commands.Bot(command_prefix=">", intents=discord.Intents.all())
 
--- music.py
+++ music.py
@@ -2,3 +2,9 @@ import discord
 from discord.ext import commands
 import youtube_dl
 
+class music(commands.Cog):
+    def _init_(self,client):
+        self.client=client 
+
+def setup(client):
+    client.add
