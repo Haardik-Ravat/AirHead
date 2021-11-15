817091c14257923d795db4488f68f696f9ff18d8
Haardik <ravat.1@iitj.ac.in>
2021-11-15 20:14:30 +0530

    ..

--- main.py
+++ main.py
@@ -2,14 +2,7 @@ import discord
 from discord.ext import commands
 import music
 import os
-
-
-
 client=commands.Bot(command_prefix=">")
-
-
-
-
 
 client.run("OTA4NTc0NzkzNDE3MjYxMDU3.YY3uNw.hmuW3WDpjZq_X9ilq4z_QkNEzmo")
 
