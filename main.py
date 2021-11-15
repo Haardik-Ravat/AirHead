20cc150f115a924bd2de37a43f836c762b2cfcc6
Haardik <ravat.1@iitj.ac.in>
2021-11-15 20:13:35 +0530

    working source

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 import music
-
-cogs=[music]
-
+import os
 
 
 
+client=commands.Bot(command_prefix=">")
 
 
-client=commands.Bot(command_prefix=">", intents=discord.Intents.all())
+
+
 
 client.run("OTA4NTc0NzkzNDE3MjYxMDU3.YY3uNw.hmuW3WDpjZq_X9ilq4z_QkNEzmo")
 
