06a3e2b41e8d7b559b97f61752f1d65abbebe05e
Haardik <ravat.1@iitj.ac.in>
2021-11-17 01:23:00 +0530

    prefix

--- main.py
+++ main.py
@@ -2,7 +2,22 @@ import discord
 from discord.ext import commands
 import music
 import os
-client=commands.Bot(command_prefix=">")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('>'):
+        await message.channel.send('Hello!')
+
+
 
 client.run("OTA4NTc0NzkzNDE3MjYxMDU3.YY3uNw.hmuW3WDpjZq_X9ilq4z_QkNEzmo")
 
